ea422d27b59da80b05a2ca08773b5d3fd62f81b1
wissalhacob <wissal.hacob@gmail.com>
2025-03-27 15:10:07 +0100

    face detection

--- frontend/src/pages/FaceDetection.js
+++ frontend/src/pages/FaceDetection.js
@@ -1,109 +1,289 @@
-import React, { useEffect, useRef, useState } from 'react';
-import * as faceapi from '@vladmandic/face-api';
+import React, { useEffect, useRef, useState } from "react";
+import * as faceapi from "@vladmandic/face-api";
+import AuthIdle from "../assets/images/auth-idle.svg";
+import AuthFace from "../assets/images/auth-face.svg";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import "./FaceDetection.css"
+function FaceDetection() {
+  const [tempAccount, setTempAccount] = useState("");
+  const [localUserStream, setLocalUserStream] = useState(null);
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  const [faceApiLoaded, setFaceApiLoaded] = useState(false);
+  const [loginResult, setLoginResult] = useState("PENDING");
+  const [imageError, setImageError] = useState(false);
+  const [counter, setCounter] = useState(5);
+  const [labeledFaceDescriptors, setLabeledFaceDescriptors] = useState({});
+  const videoRef = useRef();
+  const canvasRef = useRef();
+  const faceApiIntervalRef = useRef();
+  const videoWidth = 640;
+  const videoHeight = 360;
 
-const FaceDetection = () => {
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+
+
+  const loadModels = async () => {
+    // const uri = import.meta.env.DEV ? "/models" : "/react-face-auth/models";
+    const uri = "/models";
+
+    await faceapi.nets.ssdMobilenetv1.loadFromUri(uri);
+    await faceapi.nets.faceLandmark68Net.loadFromUri(uri);
+    await faceapi.nets.faceRecognitionNet.loadFromUri(uri);
+  };
 
   useEffect(() => {
-    // Fonction de démarrage de la vidéo
-    const startVideo = () => {
-      navigator.mediaDevices.getUserMedia({ video: { width: 720, height: 560 } })
-        .then((stream) => {
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-          }
+    setTempAccount(location?.state?.account);
+  }, []);
+  useEffect(() => {
+   
+      loadModels()
+        .then(async () => {
+          const labeledFaceDescriptors = await loadLabeledImages();
+          setLabeledFaceDescriptors(labeledFaceDescriptors);
         })
-        .catch(err => console.error('Erreur de récupération du flux vidéo', err));
-    };
-    Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-        faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
-        faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
-        faceapi.nets.faceExpressionNet.loadFromUri('./models')
-      ])
-      .then(() => {
-        console.log('Models loaded');
-        startVideo();
+        .then(() => setModelsLoaded(true));
+    
+  }, [tempAccount]);
+
+  useEffect(() => {
+    if (loginResult === "SUCCESS") {
+      const counterInterval = setInterval(() => {
+        setCounter((counter) => counter - 1);
+      }, 1000);
+
+      if (counter === 0) {
+        videoRef.current.pause();
+        videoRef.current.srcObject = null;
+        localUserStream.getTracks().forEach((track) => {
+          track.stop();
+        });
+        clearInterval(counterInterval);
+        clearInterval(faceApiIntervalRef.current);
+        localStorage.setItem(
+          "faceAuth",
+          JSON.stringify({ status: true, account: tempAccount })
+        );
+        navigate("/protected", { replace: true });
+      }
+
+      return () => clearInterval(counterInterval);
+    }
+    setCounter(5);
+  }, [loginResult, counter]);
+
+  const getLocalUserVideo = async () => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: false, video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        setLocalUserStream(stream);
       })
       .catch((err) => {
-        console.error('Error loading models:', err);
+        console.error("error:", err);
       });
-      
+  };
 
-    // Fonction pour la détection des visages lorsque la vidéo commence
-    const handlePlay = () => {
-      const video = videoRef.current;
-      const canvas = faceapi.createCanvasFromMedia(video);
-      if (canvasRef.current) {
-        canvasRef.current.appendChild(canvas);
+  const scanFace = async () => {
+    faceapi.matchDimensions(canvasRef.current, videoRef.current);
+    const faceApiInterval = setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(videoRef.current)
+        .withFaceLandmarks()
+        .withFaceDescriptors();
+      const resizedDetections = faceapi.resizeResults(detections, {
+        width: videoWidth,
+        height: videoHeight,
+      });
+
+      
+      if (!canvasRef.current) {
+        return;
       }
 
-      const displaySize = { width: video.videoWidth, height: video.videoHeight };
-      faceapi.matchDimensions(canvas, displaySize);
+      canvasRef.current
+        .getContext("2d")
+        .clearRect(0, 0, videoWidth, videoHeight);
+      faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
 
-      const detectFaces = async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions({ inputSize: 320, scoreThreshold: 0.5 }))
-          .withFaceLandmarks()
-          .withFaceExpressions();
-      
-        console.log(detections); // Afficher les résultats pour vérifier les landmarks
-      
-        if (detections.length > 0) {
-          const resizedDetections = faceapi.resizeResults(detections, displaySize);
-          canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height); // Clear previous detections
-          faceapi.draw.drawDetections(canvas, resizedDetections); // Dessiner les boîtes de détection
-          faceapi.draw.drawFaceLandmarks(canvas, resizedDetections); // Dessiner les landmarks
-          faceapi.draw.drawFaceExpressions(canvas, resizedDetections); // Dessiner les expressions
-        }
-      };
       
 
-      const intervalId = setInterval(detectFaces, 100);
-      return () => clearInterval(intervalId); // Nettoyage de l'intervalle au démontage du composant
-    };
-
-    // Ajouter un événement de lecture pour commencer la détection
-    videoRef.current?.addEventListener('play', handlePlay);
-
-    return () => {
-      // Nettoyage des ressources
-      videoRef.current?.removeEventListener('play', handlePlay);
-      if (canvasRef.current) {
-        canvasRef.current.innerHTML = ''; // Clear canvas on unmount
+      if (!faceApiLoaded) {
+        setFaceApiLoaded(true);
       }
-    };
-  }, []);
+    }, 1000 / 15);
+    faceApiIntervalRef.current = faceApiInterval;
+  };
+
+  async function loadLabeledImages() {
+    if (!tempAccount) {
+      return null;
+    }
+    const descriptions = [];
+
+    let img;
+
+    try {
+      const imgPath =
+        tempAccount?.type === "CUSTOM"
+          ? tempAccount.picture
+          : // : import.meta.env.DEV
+            // ? `/temp-accounts/${tempAccount.picture}`
+            // : `/react-face-auth/temp-accounts/${tempAccount.picture}`;
+            `/temp-accounts/${tempAccount.picture}`;
+
+      img = await faceapi.fetchImage(imgPath);
+    } catch {
+      setImageError(true);
+      return;
+    }
+
+    const detections = await faceapi
+      .detectSingleFace(img)
+      .withFaceLandmarks()
+      .withFaceDescriptor();
+
+    if (detections) {
+      descriptions.push(detections.descriptor);
+    }
+
+    return new faceapi.LabeledFaceDescriptors(tempAccount.id, descriptions);
+  }
+
+  if (imageError) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center gap-[24px] max-w-[840px] mx-auto">
+        <h2 className="text-center text-3xl font-extrabold tracking-tight text-rose-700 sm:text-4xl">
+          <span className="block">
+            Upps! There is no profile picture associated with this account.
+          </span>
+        </h2>
+        <span className="block mt-4">
+          Please contact administration for registration or try again later.
+        </span>
+      </div>
+    );
+  }
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="relative flex flex-col items-center p-[10px]">
-        <video
-          muted
-          autoPlay
-          ref={videoRef}
-          width={720}
-          height={560}
-          style={{
-            objectFit: "contain", 
-            height: "600px",
-            width: "100%",
-            borderRadius: "10px",
-          }}
-        />
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-          }}
-        />
+    <div className="h-full flex flex-col items-center justify-center gap-[24px] max-w-[720px] mx-auto">
+      {!localUserStream && !modelsLoaded && (
+        <h2 className="text-center text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span className="block">
+            You're Attempting to Log In With Your Face.
+          </span>
+          <span className="block text-indigo-600 mt-2">Loading Models...</span>
+        </h2>
+      )}
+      {!localUserStream && modelsLoaded && (
+        <h2 className="text-center text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span className="block text-indigo-600 mt-2">
+            Please Recognize Your Face to Completely Log In.
+          </span>
+        </h2>
+      )}
+      {localUserStream && loginResult === "SUCCESS" && (
+        <h2 className="text-center text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span className="block text-indigo-600 mt-2">
+            We've successfully recognize your face!
+          </span>
+          <span className="block text-indigo-600 mt-2">
+            Please stay {counter} more seconds...
+          </span>
+        </h2>
+      )}
+      {localUserStream && loginResult === "FAILED" && (
+        <h2 className="text-center text-3xl font-extrabold tracking-tight text-rose-700 sm:text-4xl">
+          <span className="block mt-[56px]">
+            Upps! We did not recognize your face.
+          </span>
+        </h2>
+      )}
+      {localUserStream && !faceApiLoaded && loginResult === "PENDING" && (
+        <h2 className="text-center text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span className="block mt-[56px]">Scanning Face...</span>
+        </h2>
+      )}
+      <div className="w-full">
+        <div className="relative flex flex-col items-center p-[10px]">
+          <video
+            muted
+            autoPlay
+            ref={videoRef}
+            height={videoHeight}
+            width={videoWidth}
+            onPlay={scanFace}
+            style={{
+              objectFit: "fill",
+              height: "360px",
+              borderRadius: "10px",
+              display: localUserStream ? "block" : "none",
+            }}
+          />
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              display: localUserStream ? "block" : "none",
+            }}
+          />
+        </div>
+        {!localUserStream && (
+          <>
+            {modelsLoaded ? (
+              <>
+                <img
+                  alt="loading models"
+                  src={AuthFace}
+                  className="cursor-pointer my-8 mx-auto object-cover h-[272px]"
+                />
+                <button
+                className="scane-button"
+                  onClick={getLocalUserVideo}
+                  type="button"                >
+                  Scanne mon visage
+                </button>
+              </>
+            ) : (
+              <>
+                <img
+                  alt="loading models"
+                  src={AuthIdle}
+                  className="model-loading"
+                />
+                <button
+                  disabled
+                  type="button"
+                  className="cursor-not-allowed flex justify-center items-center w-full py-2.5 px-5 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 inline-flex items-center"
+                >
+                  <svg
+                    aria-hidden="true"
+                    role="status"
+                    className="loading"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="#1C64F2"
+                    />
+                  </svg>
+                  Veuillez patienter pendant que les modèles se chargent...
+                </button>
+              </>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
-};
+}
 
 export default FaceDetection;
